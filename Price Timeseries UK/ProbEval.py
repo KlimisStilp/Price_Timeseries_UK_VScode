82ab9a450c9c6229d3965ba0faffd1ab909e2f0d
KlimisStilp <2836589s@student.gla.ac.uk>
2023-02-02 23:21:06 +0000

    added function to probeval

--- Price Timeseries UK/ProbEval.py
+++ Price Timeseries UK/ProbEval.py
@@ -1,5 +1,23 @@
-# Function that returns 1 or 0 if the actual observation is lower that the probability
-# on specific quantile of the predicted value
+def reliability_diagram(df, obs, mu, std):
+    from scipy.stats import norm
+    import numpy as np
+    import seaborn as sns
+    import matplotlib.pyplot as plt
+
+    x_scatter = np.arange(1,99,1)/100
+    y_scatter = []
+   
+
+    for q in x_scatter:
+        a = (df[str(obs)] < norm.ppf(q, loc=df[str(mu)], scale=df[str(std)])).to_list()
+        y_scatter.append(sum(a)/len(df))
+    
+    
+    sns.scatterplot(x=x_scatter, y=y_scatter, linestyle='dashed', color='red', marker='o')
+    plt.axline((0,0), slope=1, color='black')
+    plt.title('Reliability Diagram')
+    plt.xlabel('Nominal')
+    plt.ylabel('Observed Relative Frequency') 
 
 
 
